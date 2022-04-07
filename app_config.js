3eeec77ce647b1ff99333dee84a736e11ab10228
hlw131415 <1198842338@qq.com>
2022-04-07 10:33:13 +0800

    a

--- app_config.js
+++
@@ -1,8 +0,0 @@
-window._URL_ = {
-  API: '/api',  //后端API接口地址
-    FILE_PREFIX: '/',  //文件接口地址前缀
-  BASE_PATH:'',
-  WINDOW_EXE_PATH:'/download/3DCloudDesktop-v1.5.1.exe',
-  BESTV_PROJECT_STORAGE_DIR: 'C:/CAPTURE/hd', //存储文件目录
-  TARGET_BESTV_PROJECT_STORAGE_DIR: 'C:/4KRENDER' //目标文件存储目录
-}
